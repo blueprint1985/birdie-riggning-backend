f3ed66f227fb51d8faac3be244841c4cf316eb65
Martin Björling <martinbjorling@gmail.com>
2020-03-07 17:10:39 +0100

    Tried to fix, no success

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@
 FROM python:3.8.2
 COPY ./src /usr/src
 WORKDIR /usr/src
+RUN apt-get install default-libmysqlclient-dev
 RUN pip install -r requirements.txt
 ENTRYPOINT ["python"]
 CMD ["run.py"]
--- src/config.py
+++ src/config.py
@@ -4,4 +4,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_ECHO = False
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = "mysql://root:dinpappa@http://localhost:3306/birdierigg"
+SQLALCHEMY_DATABASE_URI = "mysql://root:dinpappa@localhost:3306/birdierigg"
